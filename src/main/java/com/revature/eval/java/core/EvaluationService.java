5232f053d1406e5cc52995019e6506cc1dedd008
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2020-01-10 00:30:39 -0500

    updated

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -138,15 +138,123 @@ public class EvaluationService {
 	 * @param string
 	 * @return
 	 */
-	// string to lowercase
-	// create a hash map with key/char and value/points
-	// create a counter
-	// for each char in string get value from hash map, add to counter
-	// return counter
+	// switch case
 	public int getScrabbleScore(String string) {
-		// TODO Write an implementation for this method declaration
 
-		return 0;
+	    int points = 0;
+	    char letter;
+		char[] token = string.toUpperCase().toCharArray();
+
+		for (char letters : token) {
+			switch (letter) {
+
+			case 'A':
+				points += 1;
+				break;
+				
+			case 'B':
+				points += 3;
+				break;
+				
+			case 'C':
+				points += 3;
+				break;
+				
+			case 'D':
+				points += 2;
+				break;
+				
+			case 'E':
+				points += 1;
+				break;
+				
+			case 'F':
+				points += 4;
+				break;
+				
+			case 'G':
+				points += 2;
+				break;
+				
+			case 'H':
+				points += 4;
+				break;
+				
+			case 'I':
+				points += 1;
+				break;
+				
+			case 'J':
+				points += 8;
+				break;
+				
+			case 'K':
+				points += 5;
+				break;
+				
+			case 'L':
+				points += 1;
+				break;
+				
+			case 'M':
+				points += 3;
+				break;
+				
+			case 'N':
+				points += 1;
+				break;
+				
+			case 'O':
+				points += 1;
+				break;
+				
+			case 'P':
+				points += 3;
+				break;
+				
+			case 'Q':
+				points += 10;
+				break;
+				
+			case 'R':
+				points += 1;
+				break;
+				
+			case 'S':
+				points += 1;
+				break;
+				
+			case 'T':
+				points += 1;
+				break;
+				
+			case 'U':
+				points += 1;
+				break;
+				
+			case 'V':
+				points += 4;
+				break;
+				
+			case 'W':
+				points += 4;
+				break;
+				
+			case 'X':
+				points += 8;
+				break;
+				
+			case 'Y':
+				points += 4;
+				break;
+				
+			case 'Z':
+				points += 10;
+				break;
+			}
+		}
+
+		return points;
 	}
 
 	/**
@@ -278,6 +386,7 @@ public class EvaluationService {
 			if (!(t instanceof Comparable)) {
 				throw new IllegalArgumentException("Can't compare");
 			}
+			
 			@SuppressWarnings("unchecked")
 			List<Comparable> list = (List<Comparable>) sortedList;
 			Comparable item = (Comparable) t;
@@ -288,19 +397,21 @@ public class EvaluationService {
 			while (l <= r) {
 
 				int c = (l + r) / 2;
-				int compareResult = item.compareTo(list.get(c));
+				int compare = item.compareTo(list.get(c));
 
-				if (compareResult == 0) {
+				if (compare == 0) {
 					return c;
 				}
-				if (compareResult < 0) {
+				
+				if (compare < 0) {
 					r = c - 1;
 				} else {
 					l = c + 1;
 				}
 			}
-
+			
 			return -1;
+			
 		}
 
 		public BinarySearch(List<T> sortedList) {
